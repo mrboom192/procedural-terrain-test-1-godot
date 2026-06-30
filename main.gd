e49cb04e53c2bdfebb29992b7515027a72ba0e4d
Jason Nguyen <mrboom192@gmail.com>
2026-06-29 23:58:37 -0500

    small optimizations

--- main.gd
+++ main.gd
@@ -1,9 +1,9 @@
 extends Node
 
 @export var ground_tile: PackedScene
-var tile_size: float = 100.0;
+var tile_size: float = 100.0
 var generated := {}
-var max := 1000
+var max := 5000
 var subdivide_levels = 200
 
 func generate_terrain() -> void:
@@ -34,11 +34,11 @@ func generate_terrain() -> void:
 		generated[pos] = true
 		
 		var ground = ground_tile.instantiate()
-		var distance_from_center = clamp(sqrt((pos.x) ** 2 + (pos.z) ** 2) / 50, 1.0, 1000.0)
+		var distance_from_center = sqrt((pos.x) ** 2 + (pos.z) ** 2)
 		
 		ground.subdivide_levels = max(
 			8,
-			int(subdivide_levels / distance_from_center)
+			int(subdivide_levels / clamp(distance_from_center / 50, 1.0, 1000.0))
 		)
 		
 		ground.tile_size = tile_size
@@ -50,10 +50,22 @@ func generate_terrain() -> void:
 		
 		add_child(ground)
 		
-		queue.push_back(Vector3(pos.x + tile_size, 0.0, pos.z))
-		queue.push_back(Vector3(pos.x - tile_size, 0.0, pos.z))
-		queue.push_back(Vector3(pos.x, 0.0, pos.z + tile_size))
-		queue.push_back(Vector3(pos.x, 0.0, pos.z - tile_size))
+		var up = Vector3(pos.x, 0.0, pos.z - tile_size)
+		var down = Vector3(pos.x, 0.0, pos.z + tile_size)
+		var left = Vector3(pos.x - tile_size, 0.0, pos.z)
+		var right = Vector3(pos.x + tile_size, 0.0, pos.z)
+		
+		if !generated.has(up):
+			queue.push_back(up)
+		
+		if !generated.has(down):
+			queue.push_back(down)
+			
+		if !generated.has(left):
+			queue.push_back(left)
+			
+		if !generated.has(right):
+			queue.push_back(right)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
